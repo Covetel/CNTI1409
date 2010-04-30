36a5649b3563aaedbd843576cd2c1f4de3b1544f
Walter Vargas <walter@covetel.com.ve>
2010-04-30 03:04:06 -0430

    Agregando:  * Hoja de estilos formularios.css  * Archivo de configuración del formulario de pruebas "formularios/formulario.yml"  * Controladora que genera el formulario a partir el fichero de configuración

---
+++ CNTI1409/lib/CNTI1409/Controller/Formularios.pm
@@ -0,0 +1,52 @@
+package CNTI1409::Controller::Formularios;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller::HTML::FormFu'; }
+
+=head1 NAME
+
+CNTI1409::Controller::Formularios - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched CNTI1409::Controller::Formularios in Formularios.');
+}
+
+sub formulario : Local : FormConfig {
+    	my ( $self, $c ) = @_;
+	my $form = $c->stash->{form};
+        $c->stash->{template} = 'formulario.tt2';
+        if ($form->submitted_and_valid) {
+                $c->res->body("Formulario enviado exitosamente");
+        }
+}
+
+
+=head1 AUTHOR
+
+,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
