a78de1292236908f412c2c3bd26120422b1d2c75
Jose Rey <reyjose@gmail.com>
2010-05-09 12:17:12 -0430

    Agregar librería para usar libxml2

---
+++ CNTI-Validator/lib/CNTI/Validator/LibXML.pm
@@ -0,0 +1,52 @@
+package CNTI::Validator::LibXML;
+use Moose;
+use XML::LibXML '1.69';
+
+has xpc => ( is => 'ro' );
+has doc => ( is => 'ro' );
+
+around BUILDARGS => sub {
+    $DB::single = 1;
+    my $orig  = shift;
+    my $class = shift;
+    my %args  = @_;
+    unless ( $args{'doc'} ) {
+        $args{'doc'} = sub {
+
+            # Create and configure the parser.
+            my $parser = XML::LibXML->new;
+
+            # Apply any parser options.
+            if ( my $opts = delete $args{'options'} ) {
+                while ( my ( $k, $v ) = each %{$opts} ) {
+                    $parser->$k($v);
+                }
+            }
+
+            # Parse and return the document.
+            if ( my $xml = delete $args{'xml'} ) {
+                return delete $args{'is_html'}
+                    ? $parser->parse_html_string($xml)
+                    : $parser->parse_string($xml);
+            }
+
+            if ( my $file = delete $args{'file'} ) {
+                return delete $args{'is_html'}
+                    ? $parser->parse_html_file($file)
+                    : $parser->parse_file($file);
+            }
+
+            require Carp;
+            Carp::carp('Test::XPath->new requires the "xml", "file", or "doc" parameter');
+        }->();
+    }
+    $args{'xpc'} = XML::LibXML::XPathContext->new( $args{'doc'}->documentElement );
+    if ( my $ns = delete $args{'xmlns'} ) {
+        while ( my ( $k, $v ) = each %{$ns} ) {
+            $args{'xpc'}->registerNs( $k => $v );
+        }
+    }
+    return \%args;
+};
+
+1;
