e889ba169e49958e3d7d158590b93732f58d44dc
Juan Mesa (overdrive) <juan@covetel.com.ve>
2010-05-10 01:31:35 +0000

    Creadas las validaciones que indican si una entidad o institucion existe o no

---
+++ CNTI1409/lib/HTML/FormFu/Validator/CNTI1409/Existe/Institucion.pm
@@ -0,0 +1,41 @@
+package HTML::FormFu::Validator::CNTI1409::Existe::Institucion;
+#
+#===============================================================================
+#
+#         FILE:  Insitucion.pm
+#
+#  DESCRIPTION:  Validador de FormFu que verifica si una institucion existe antes
+#                de que una auditoria sea creada
+#
+#        FILES:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  overdrive (), juan@covetel.com.ve
+#      COMPANY:  COVETEL R.S.
+#      VERSION:  1.0
+#      CREATED:  05/09/10 23:06:07
+#     REVISION:  ---
+#===============================================================================
+
+use strict;
+use warnings;
+use base 'HTML::FormFu::Validator';
+use utf8;
+
+sub validate_value {
+    my ( $self, $value, $params ) = @_;
+
+    my $c = $self->form->stash->{context};
+
+    my $idinstitucion = $c->model('DB::Institucion')->find(
+                                                            { "lower(me.nombre)" => lc($value), habilitado => "true" },
+                                                            { columns => [ qw / id / ] }
+                                                          );
+    return 1 if $idinstitucion;
+
+    die HTML::FormFu::Exception::Validator->new({
+        message => 'La institución no existe, por favor verifique',
+    });
+}
+
+1;
