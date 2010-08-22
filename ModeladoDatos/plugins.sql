fe290da6790c580d340d93c33897175afbf8ad8a
overdrive <overdrive@overlaptop.(none)>
2010-08-22 07:47:38 +0000

    Script para agregar la nueva disposición a la base de datos

---
+++ ModeladoDatos/plugins.sql
@@ -0,0 +1,2 @@
+-- Insertar datos de la nueva disposicón
+INSERT INTO disposicion (nombre, descripcion, habilitado, modulo) VALUES ('Controles Añadibles', 'Existencia de controles añadibles de tipo ActiveX', TRUE, 'Plugins');
