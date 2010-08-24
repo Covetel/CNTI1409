0183d92c5f438ca510b5ad36ab790e6cd4585be5
Walter Vargas <walter@covetel.com.ve>
2010-08-24 19:28:23 -0430

    Agregadas las primeras pruebas para el reporte en PDF

---
+++ CNTI1409/lib/CNTI1409/View/PDF.pm
@@ -0,0 +1,41 @@
+package CNTI1409::View::PDF;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config({
+	TEMPLATE_EXTENSION => '.tt2',
+	ENCODING     => 'UTF-8',
+    CATALYST_VAR => 'c',
+	INCLUDE_PATH => [
+        CNTI1409->path_to( 'root', 'src', 'reportes' ),
+	],
+
+});
+
+=head1 NAME
+
+CNTI1409::View::PDF - TT View for CNTI1409
+
+=head1 DESCRIPTION
+
+TT View for CNTI1409.
+
+=head1 SEE ALSO
+
+L<CNTI1409>
+
+=head1 AUTHOR
+
+,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
