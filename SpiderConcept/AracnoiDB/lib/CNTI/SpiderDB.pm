61c96cf710671394ff45cbb05861a6943596b76e
Jose Rey <reyjose@gmail.com>
2010-08-29 21:11:58 -0430

    Capa de persistencia
    Se implementa un proyecto en catalyst para hacer un modelito
    para la persistencia de las tareas de la araña

---
+++ SpiderConcept/AracnoiDB/lib/CNTI/SpiderDB.pm
@@ -0,0 +1,19 @@
+package CNTI::SpiderDB;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+
+# Created by DBIx::Class::Schema::Loader v0.07001 @ 2010-08-29 19:43:31
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:86ASFJjp8+IxnGvCcKi3Eg
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
