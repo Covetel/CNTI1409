255d877d10abac2558cd67458c0c1843c25596c7
Walter Vargas <walter@covetel.com.ve>
2010-10-19 04:56:56 -0430

    /reportes Redirecciona a Login cuando no esta autenticado

--- CNTI1409/lib/CNTI1409/Controller/Reportes.pm
+++ CNTI1409/lib/CNTI1409/Controller/Reportes.pm
@@ -20,6 +20,23 @@ Esta controladora es responsable de generar todos los reportes del sistema.
 
 =cut
 
+=head2 auto
+
+Redirecciona a Login.
+
+=cut
+
+sub auto :Private {
+    my ( $self, $c ) = @_;
+    if ($c->controller eq $c->controller('Root')->action_for('login')) {
+        return 1;
+    }
+    if (!$c->user_exists) {
+        $c->response->redirect($c->uri_for('/login'));
+        return 0;
+    }
+    return 1;
+}
 
 =head2 index
 
