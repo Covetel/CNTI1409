16e5de9ec99daea35d107ab61f814a2a1de55f55
Walter Vargas <walter@covetel.com.ve>
2010-10-20 21:02:43 -0430

    Script para probar el servicio SOAP

--- CNTI1409/lib/CNTI1409/SOAP/Server.pm
+++ CNTI1409/lib/CNTI1409/SOAP/Server.pm
@@ -9,14 +9,19 @@ has 'server' => (is => 'ro', isa => 'SOAP::Transport::HTTP::Daemon', lazy => 1, 
 
 sub _server_soap {
 	my ($self) = @_;
-	return SOAP::Transport::HTTP::Daemon->new(LocalAddr => $self->address, LocalPort => $self->port)->dispatch_to($self->clase); 
-	
+	return SOAP::Transport::HTTP::Daemon->new(LocalAddr => $self->address, ReuseAddr => 1, LocalPort => $self->port)->dispatch_to($self->clase); 
 }
 
 sub start {
 	my ($self) = @_;
 	$self->server;
 	$self->server->handle();
+}
+
+sub uri {
+	my ($self) = @_;
+	print "prueba";
+	return $self->server->myuri();
 }
 
 1;
--- CNTI1409/lib/soap_test.pl
+++ CNTI1409/lib/soap_test.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use CNTI1409::SOAP::Server; 
 
+print "La direccion es : ";
 my $servidor = CNTI1409::SOAP::Server->new(address => 'localhost', port => '8083');
 $servidor->clase('CNTI1409::SOAP::Auditorias');
 $servidor->start;
