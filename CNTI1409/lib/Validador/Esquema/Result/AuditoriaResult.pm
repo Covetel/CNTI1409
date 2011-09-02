27a5c453fc39fc317e568a36c43a754b043f5c95
Walter Vargas <walter@covetel.com.ve>
2011-09-01 20:20:21 -0430

    Actualizando el resultset AuditoriaResult, id ahora es serial

--- CNTI1409/lib/Validador/Esquema/Result/AuditoriaResult.pm
+++ CNTI1409/lib/Validador/Esquema/Result/AuditoriaResult.pm
@@ -20,11 +20,6 @@ __PACKAGE__->table("auditoria_result");
 
 =head1 ACCESSORS
 
-=head2 id
-
-  data_type: 'integer'
-  is_nullable: 0
-
 =head2 id_auditoria
 
   data_type: 'integer'
@@ -36,17 +31,28 @@ __PACKAGE__->table("auditoria_result");
   data_type: 'text'
   is_nullable: 1
 
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+  sequence: 'auditoria_result_id_seq'
+
 =cut
 
 __PACKAGE__->add_columns(
-  "id",
-  { data_type => "integer", is_nullable => 0 },
   "id_auditoria",
   { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
   "json",
   { data_type => "text", is_nullable => 1 },
+  "id",
+  {
+    data_type         => "integer",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "auditoria_result_id_seq",
+  },
 );
-__PACKAGE__->set_primary_key("id");
 
 =head1 RELATIONS
 
@@ -66,9 +72,31 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07010 @ 2011-07-23 19:38:22
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:UBT0TbvHmoyJQi2viBErwg
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2011-09-01 20:19:36
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:qzFtOkG/Egg94nVYuJN0Cg
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
+
+=head2 to_json 
+
+Recibe el hash y lo serializa a json.
+
+=cut 
+    
+
+use JSON::XS;
+
+sub to_json {
+    my $self = shift;
+    my $json = encode_json $self->json;
+    return $json;
+}
+
+sub json {
+    my $self = shift;
+    my $json = decode_json $self->json;
+    return $json;
+}
+
 1;
