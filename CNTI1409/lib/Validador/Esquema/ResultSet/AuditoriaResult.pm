f2b5d8f4f396066093d6948e9d168931268b482d
Walter Vargas <walter@covetel.com.ve>
2011-10-02 22:31:54 -0430

    ResultClass Resultados JSON

---
+++ CNTI1409/lib/Validador/Esquema/ResultSet/AuditoriaResult.pm
@@ -0,0 +1,7 @@
+package Validador::Esquema::ResultSet::AuditoriaResult;
+use strict;
+use warnings;
+use base 'DBIx::Class::ResultSet';
+
+  
+1;
---
+++ CNTI1409/t/view_Ajax.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::More;
+
+BEGIN { use_ok 'CNTI1409::View::Ajax' }
+
+done_testing();
